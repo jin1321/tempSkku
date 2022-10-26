6bf88773d187acf568bbbfad92ce0e9e131f532b
wonleeeee <won8303@gmail.com>
2022-10-25 22:35:19 -0400

    changed db

--- skku/backend/app/config.js
+++ skku/backend/app/config.js
@@ -1,14 +1,17 @@
 const mysql = require("mysql2");
+const pw = require("./pw")
 
 var config = module.exports;
 
 config.db = mysql.createPool({
-    user: "root",
-    host: "127.0.0.1",
-    password: `Mangoleahchoi3!`,
+    user: "admin",
+    host: "skkuboardserver.czlefodezyvn.ap-northeast-2.rds.amazonaws.com",
+    password: `${pw}`,
     database: "mydb",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
 });
 
 config.express = {
-    port: 3001
-};
